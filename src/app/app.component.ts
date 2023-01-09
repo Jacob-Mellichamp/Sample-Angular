c338419efe01a31b58e6a4a525900c57a21a9f0c
Jacob Mellichamp <jakemellichamp@gmail.com>
2023-01-09 14:00:06 -0500

    Customized App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,8 +13,9 @@ import { Component } from "@angular/core";
   <div class='container'>
     <router-outlet></router-outlet>
   </div>
-  `
+  `,
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  pageTitle: string = 'Acme Product Management';
+  pageTitle: string = 'Angular Website';
 }
